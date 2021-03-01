c28d48648e4b2e04dd8e32ba4eb64e9fa8e8d2ad
James R T <jamestiotio@gmail.com>
2021-03-01 11:39:14 +0800

    Add week 6 cohort 1 code

--- cohort/week06/SQLInjectionCompliant.java
+++ cohort/week06/SQLInjectionCompliant.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 
 public class SQLInjectionCompliant {
 	String hashPassword(char[] password) {
-		// create hash of password
-		return null;
+		// Create hash of password
+		return password.toString();
 	}
 
 	public void doPrivilegedAction(String username, char[] password) throws SQLException {
@@ -20,11 +20,14 @@ public class SQLInjectionCompliant {
 			if (username.length() > 8) {
 				// Handle error
 			}
+			// Possible SQL injection patterns that could succeed here (if without the set*() methods below):
+			// - validuser' OR '0'='0
+			// - \”1' or '1' = '1’ /*\”
 			String sqlString = "select * from db_user where username=? and password=?";
 			PreparedStatement stmt = connection.prepareStatement(sqlString);
-			//Use the set*() methods of the PreparedStatement class to enforce strong type checking.
-			//This mitigates the SQL injection vulnerability because the input is properly escaped by
-			//automatic entrapment within double quotes.
+			// Use the set*() methods of the PreparedStatement class to enforce strong type checking.
+			// This mitigates the SQL injection vulnerability because the input is properly escaped by
+			// automatic entrapment within double quotes.
 			stmt.setString(1, username);
 			stmt.setString(2, pwd);
 			ResultSet rs = stmt.executeQuery();
@@ -42,8 +45,9 @@ public class SQLInjectionCompliant {
 		}
 	}
 
-	private Connection getConnection() {
-		// TODO Auto-generated method stub
-		return null;
+	private Connection getConnection() throws SQLException {
+		DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+		String dbConnection = PropertyManager.getProperty("db.connection");
+		return DriverManager.getConnection(dbConnection);
 	}
 }

39c23120bdea444184bc50edf6cf3fa0dda3299f
James R T <jamestiotio@gmail.com>
2021-03-17 17:28:55 +0800

    Add week 9 cohort starter code

---
+++ cohort/week09/DLExample/Point.java
@@ -0,0 +1,4 @@
+package Week9.DLExample;
+public class Point {
+
+}
---
+++ cohort/week09/DLExample/Taxi.java
@@ -0,0 +1,24 @@
+package Week9.DLExample;
+
+public class Taxi {
+    private Point location, destination;
+    private final Dispatcher dispatcher;
+
+    public Taxi(Dispatcher dispatcher) {
+        this.dispatcher = dispatcher;
+    }
+
+    public synchronized Point getLocation() {
+        return location;
+    }
+
+    public synchronized void setLocation(Point location) {
+        this.location = location;
+        if (location.equals(destination))
+            dispatcher.notifyAvailable(this);
+    }
+
+    public synchronized Point getDestination() {
+        return destination;
+    }
+}

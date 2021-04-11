5889b41215f81aa8608a804c484aa270a58d494e
James R T <jamestiotio@gmail.com>
2021-04-11 23:35:36 +0800

    Update FirstExampleFixed with alternative solution

--- cohort/week10/FirstExampleFixed.java
+++ cohort/week10/FirstExampleFixed.java
@@ -1,9 +1,14 @@
 import java.util.Random;
 import java.util.Vector;
+import java.util.concurrent.locks.ReentrantLock;
 
 // Please do take note that generally, it is not recommended to rely on method parameters for
-// synchronization (such as by locking about/on them and using them as locks)
+// synchronization (such as by locking about/on them and using them as locks). A possibly better
+// idea would be to lock on either this object or some form of a global lock (such as a
+// ReentrantLock).
 public class FirstExampleFixed {
+    // private static ReentrantLock lock = new ReentrantLock();
+
     public static void main(String[] args) throws InterruptedException {
         Vector<Integer> v = new Vector<>(10);
         int num_threads = 2;
@@ -16,7 +21,7 @@ public class FirstExampleFixed {
         for (int i = 0; i < num_threads; i++) {
             test[i] = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
-                    // if (r.nextBoolean()) {  // This is indeterminate
+                    // if (r.nextBoolean()) { // This is indeterminate
                     if (j % 2 == 0) {
                         getLast(v);
                     } else {
@@ -41,29 +46,37 @@ public class FirstExampleFixed {
 
     public static Object getLast(Vector list) {
         synchronized (list) {
+            // lock.lock();
             int lastIndex = list.size() - 1;
-            return list.get(lastIndex);
+            Object toReturn = list.get(lastIndex);
+            // lock.unlock();
+            return toReturn;
         }
     }
 
     public static void deleteLast(Vector list) {
         synchronized (list) {
+            // lock.lock();
             int lastIndex = list.size() - 1;
             list.remove(lastIndex);
+            // lock.unlock();
         }
     }
 
     public static boolean contains(Vector list, Object obj) {
+        // Possibility of deadlock here if using synchronized keywords. Might want to implement
+        // using a global ReentrantLock instead.
         synchronized (list) {
-            // Possibility of deadlock here? Might want to implement using a global ReentrantLock
-            // instead.
             synchronized (obj) {
+                // lock.lock();
                 for (int i = 0; i < list.size(); i++) {
                     if (list.get(i).equals(obj)) {
+                        // lock.unlock();
                         return true;
                     }
                 }
 
+                // lock.unlock();
                 return false;
             }
         }

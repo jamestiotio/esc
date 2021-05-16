98017e46738b01257f3345b281bb36e45125cc7e
James R T <jamestiotio@gmail.com>
2021-05-16 08:02:50 +0800

    Update some code

--- cohort/week10/ExperimentFixed2.java
+++ cohort/week10/ExperimentFixed2.java
@@ -1,10 +1,13 @@
+import java.util.concurrent.atomic.AtomicInteger;
 // import java.util.concurrent.locks.ReentrantLock;
 
 public class ExperimentFixed2 {
-    private static int MY_INT = 0;
+    // Either using AtomicInteger or a separate lock object should be fine. There is no need to use
+    // both.
+    private static AtomicInteger MY_INT = new AtomicInteger(0);
     // More memory is needed to be allocated to create the lock, and in terms of time, each thread
     // needs to wait until the lock is released before it can access it
-    private static Object lock = new Object();  // A ReentrantLock could also be used
+    private static Object lock = new Object(); // A ReentrantLock could also be used
     // public static ReentrantLock mutex = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
@@ -17,13 +20,18 @@ public class ExperimentFixed2 {
 
     static class ChangeListener extends Thread {
         public void run() {
-            int local_value = MY_INT;
-            while (local_value < 5) {
+            AtomicInteger local_value;
+            synchronized (lock) {
+                // ExperimentFixed2.mutex.lock();
+                local_value = new AtomicInteger(MY_INT.get());
+                // ExperimentFixed2.mutex.unlock();
+            }
+            while (local_value.get() < 5) {
                 synchronized (lock) {
                     // ExperimentFixed2.mutex.lock();
-                    if (local_value != MY_INT) {
-                        System.out.println("Got Change for MY_INT : " + MY_INT);
-                        local_value = MY_INT;
+                    if (local_value.get() != MY_INT.get()) {
+                        System.out.println("Got Change for MY_INT : " + MY_INT.get());
+                        local_value = new AtomicInteger(MY_INT.get());
                     }
                     // ExperimentFixed2.mutex.unlock();
                 }
@@ -33,12 +41,17 @@ public class ExperimentFixed2 {
 
     static class ChangeMaker extends Thread {
         public void run() {
-            int local_value = MY_INT;
-            while (MY_INT < 5) {
+            AtomicInteger local_value;
+            synchronized (lock) {
+                // ExperimentFixed2.mutex.lock();
+                local_value = new AtomicInteger(MY_INT.get());
+                // ExperimentFixed2.mutex.unlock();
+            }
+            while (MY_INT.get() < 5) {
                 synchronized (lock) {
                     // ExperimentFixed2.mutex.lock();
-                    System.out.println("Incrementing MY_INT to " + (local_value + 1));
-                    MY_INT = ++local_value;
+                    System.out.println("Incrementing MY_INT to " + (local_value.get() + 1));
+                    MY_INT = new AtomicInteger(local_value.incrementAndGet());
                     // ExperimentFixed2.mutex.unlock();
                 }
                 try {
